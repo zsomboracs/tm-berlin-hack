49c6097034ee6b046fcf2255895873317a1e9901
Peter Kedvessy <v-pkedvessy@expedia.com>
2017-01-28 13:38:29 +0100

    Removes hotel service

--- hotel-event-service/src/main/java/com/hotels/service/HotelService.java
+++ hotel-event-service/src/main/java/com/hotels/service/HotelService.java
@@ -1,27 +1,17 @@
 package com.hotels.service;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
-import javax.inject.Inject;
-
+import org.omg.CORBA.Object;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import com.hotels.api.Hotel;
-import com.hotels.api.HotelSearchResult;
-import com.hotels.client.HotelSearchClient;
-import com.hotels.client.domain.Address;
-import com.hotels.client.domain.Body;
-import com.hotels.client.domain.Data;
-import com.hotels.client.domain.Result;
-import com.hotels.client.domain.SearchResults;
+import com.hotels.domain.hotel.HotelSearchResult;
 import com.hotels.web.HotelSearchForm;
 
 /**
@@ -30,81 +20,28 @@ import com.hotels.web.HotelSearchForm;
 @Service
 public class HotelService {
 
-    private static final String FALLBACK_DESTINATION = "New York";
-    private static final int MIN_ADUlTS = 1;
-    private static final int MAX_ADUlTS = 8;
+    @Autowired
+    private RestTemplate restTemplateHotel;
+    @Autowired
+    private HttpEntity httpEntity;
 
-    @Inject
-    private HotelSearchClient client;
-    @Inject
-    private DestinationService destinationService;
+    @Value("${TICKETMACHINE_API}")
+    private String apiUrl;
 
     public HotelSearchResult search(HotelSearchForm form) throws IOException {
-        Optional<String> resolvedDestination = destinationService.resolveDestination(form.getLatitude(), form.getLongitude());
-        Optional<String> fallbackDestination = Optional.ofNullable(form.getDestination());
-        String destination = resolvedDestination.orElse(fallbackDestination.orElse(FALLBACK_DESTINATION));
-        LocalDate checkIn = getValidCheckIn(form.getCheckIn(), LocalDate.now());
-        LocalDate checkOut = getValidCheckOut(form.getCheckOut(), checkIn);
-        String coordinates = getCoordinates(form.getLatitude(), form.getLongitude());
-        Optional<com.hotels.client.domain.HotelSearchResult> searchResult
-            = client.getSearchResult(coordinates, destination, checkIn, checkOut, getValidAdults(form.getAdults()));
-        List<Result> results = searchResult.map(com.hotels.client.domain.HotelSearchResult::getData)
-            .map(Data::getBody)
-            .map(Body::getSearchResults)
-            .map(SearchResults::getResults)
-            .orElse(Collections.emptyList());
-
-        return new HotelSearchResult(destination, results.stream().map(this::createHotel).filter(Objects::nonNull).collect(Collectors.toList()));
+        HttpEntity<HotelSearchResult> response = restTemplateHotel.exchange(getUrl(form), HttpMethod.GET, httpEntity, HotelSearchResult.class);
+        return response.getBody();
     }
 
-    private Hotel createHotel(Result result) {
-        try {
-            Hotel hotel = new Hotel();
-            hotel.setName(result.getName());
-            hotel.setImage(result.getThumbnailUrl().replace("https", "http").replace("_l.jpg", "_b.jpg"));
-            hotel.setPrice(result.getRatePlan().getPrice().getCurrent());
-            hotel.setUrl("https://www.hotels.com" + result.getUrls().getPdpDescription() + "&pos=HCOM_US#rooms-and-rates-anchor");
-            hotel.setStarRating(result.getStarRating());
-            hotel.setGuestRating(result.getGuestReviews().getRating());
-            hotel.setTripAdvisorRating(result.getTripAdvisorGuestReviews().getRating());
-            hotel.setPriceAmount(result.getRatePlan().getPrice().getExactCurrent());
-            hotel.setAddress(getAddress(result));
-            return hotel;
-        } catch (Exception ex) {
-            return null;
-        }
-    }
-
-    private int getValidAdults(int adults) {
-        return Math.min(Math.max(adults, MIN_ADUlTS), MAX_ADUlTS);
-    }
-
-    private LocalDate getValidCheckIn(LocalDate checkIn, LocalDate today) {
-        if (checkIn == null || checkIn.isBefore(today)) {
-            return today;
-        }
-        return checkIn;
-    }
-
-    private LocalDate getValidCheckOut(LocalDate checkOut, LocalDate checkIn) {
-        if (checkOut == null) {
-            return checkIn.plus(1, ChronoUnit.DAYS);
-        }
-        return checkOut;
-    }
-
-    private String getCoordinates(double latitude, double longitude) {
-        return Double.toString(latitude) + "+" + Double.toString(longitude);
-    }
-
-    private String getAddress(Result result) {
-        List<String> addressElements = new ArrayList<>();
-        Optional<Address> address = Optional.ofNullable(result.getAddress());
-        address.map(Address::getStreetAddress).ifPresent(addressElements::add);
-        address.map(Address::getLocality).ifPresent(addressElements::add);
-        address.map(Address::getRegion).ifPresent(addressElements::add);
-        address.map(Address::getCountryName).ifPresent(addressElements::add);
-
-        return String.join(", ", addressElements);
+    private String getUrl(HotelSearchForm form) {
+        return UriComponentsBuilder
+            .fromUriString(apiUrl)
+            .queryParam("adults", form.getAdults())
+            .queryParam("checkIn", form.getCheckIn())
+            .queryParam("checkOut", form.getCheckOut())
+            .queryParam("destination", form.getDestination())
+            .queryParam("latitude", form.getLatitude())
+            .queryParam("longitude", form.getLongitude())
+            .toUriString();
     }
 }
